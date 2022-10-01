a4e13ba82fc9983cd598aa4a69300c00b3ca49d1
FonzyLima <71457986+FonzyLima@users.noreply.github.com>
2022-10-01 22:11:50 +0800

    Update Detection.cs

--- Unity/Detection.cs
+++ Unity/Detection.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class Detection : MonoBehaviour
 {
+    public Transform player;
+    public GameObject alerts;
     // Start is called before the first frame update
     void Start()
     {
@@ -12,7 +14,14 @@ public class Detection : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
+    {   
+    
+        if(player.rotation.z < -0.25){
+            alerts.SetActive(true);
+        }
+        else{
+            alerts.SetActive(false);
+        }
         
     }
 }
